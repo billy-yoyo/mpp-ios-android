b6ee4f115823cf7f2dcd6b4e61ef7cdcb66746d0
Will Page <billyoyo@hotmail.co.uk>
2020-09-11 15:57:15 +0100

    add tickets table to ios

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -8,9 +8,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var departureStationLabel: UILabel!
     @IBOutlet weak var arrivalStationLabel: UILabel!
     @IBOutlet weak var getTimesButton: UIButton!
+    @IBOutlet weak var ticketTable: UITableView!
     
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
     private var stationData: [Station] = [];
+    private var ticketData: [TicketInfo] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +52,44 @@ extension ViewController :  UIPickerViewDelegate, UIPickerViewDataSource {
     }
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func formatDateTime(datetime: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd'T'HH:mm:ss.SSSXXX"
+        guard let date: Date = dateFormatter.date(from: datetime) else {
+            return datetime
+        }
+        
+        let prettyDateFormatter = DateFormatter()
+        prettyDateFormatter.dateFormat = "MMM d, h:mm a"
+        return prettyDateFormatter.string(from: date)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ticketData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "TicketTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TicketTableViewCell else {
+            fatalError("The dequeued cell is not an instance of TicketTableViewCell")
+        }
+        let ticket = ticketData[indexPath.row]
+        
+        cell.departureTime.text = "Departs \(formatDateTime(datetime: ticket.departureTime))"
+        cell.arrivalTime.text = "Arrives \(formatDateTime(datetime: ticket.arrivalTime))"
+        cell.priceRange.text = "From £\(ticket.minPrice / 100) to £\(ticket.maxPrice / 100)"
+        
+        return cell
+    }
+    
+    
+}
+
 extension ViewController: ApplicationContractView {
     func setStations(stations: [Station]) {
         stationData = stations;
@@ -76,5 +116,18 @@ extension ViewController: ApplicationContractView {
         }
         
         UIApplication.shared.open(link)
+    }
+    
+    func clearTickets() {
+        ticketData.removeAll()
+        
+        ticketTable.reloadData()
+    }
+    
+    func addTicket(ticket: TicketInfo) {
+        print(ticket.departureTime)
+        ticketData.append(ticket)
+        
+        ticketTable.reloadData()
     }
 }
