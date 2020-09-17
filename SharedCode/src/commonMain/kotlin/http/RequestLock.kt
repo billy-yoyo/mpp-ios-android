8ade35965054f5f9de6de9491f01287b572441e3
Will Page <billyoyo@hotmail.co.uk>
2020-09-17 10:08:31 +0100

    add documentation to attempt function

--- SharedCode/src/commonMain/kotlin/http/RequestLock.kt
+++ SharedCode/src/commonMain/kotlin/http/RequestLock.kt
@@ -15,15 +15,26 @@ class RequestLock {
         return RequestKey(key)
     }
 
-    fun claim(): RequestKey {
+    private fun claim(): RequestKey {
         key = createRandomKey()
         return key!!
     }
 
-    fun check(key: RequestKey): Boolean {
+    private fun check(key: RequestKey): Boolean {
         return key.id == this.key?.id
     }
 
+    /**
+     * Attempts to run a function and return its result, but will return null
+     * if another function is started using this lock before this one finishes.
+     *
+     * This is useful if you have a scenario where you're making repeated potentially
+     * overlapping network requests and only ever want the latest one to complete.
+     *
+     * @param T the return type of the higher order function
+     * @property func the function we are attempting to run
+     * @return the result of func, if it completes before another func is attempted, else null
+     */
     suspend fun <T> attempt(func: suspend () -> T): T? {
         val key = claim()
         val result = func()
